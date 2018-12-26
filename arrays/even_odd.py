6b4d91fc57732d240b094c84739283e0c8427dc2
felix990302 <felix990302@yahoo.ca>
2018-12-25 23:21:41 -0800

    added even_odd sort

---
+++ arrays/even_odd.py
@@ -0,0 +1,9 @@
+# reorder array so even numbers come before odd numbers O(n)
+def even_odd(A):
+    next_even, next_odd = 0, len(A)-1
+    while next_even < next_odd:
+        if A[next_even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next_even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
