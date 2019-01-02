65849d5b41c680329d1e094b5a2cf087f1a20e85
felix990302 <felix990302@yahoo.ca>
2019-01-01 23:52:12 -0800

    non attacking placments of n queens on a nxn board

---
+++ recursion/n_non_attacking_queens.py
@@ -0,0 +1,13 @@
+def n_queens(n):
+    def solve_n_queens(row):
+        if row == n:
+            result.append(list(col_placement))
+            return
+        for col in range(n):
+            if all(abs(c-col) not in (0, row-i) for i, c in enumerate(col_placement[:row])):
+                col_placement[row] = col
+                solve_n_queens(row+1)
+
+    result, col_placement = [], [0]*n
+    solve_n_queens(0)
+    return result
