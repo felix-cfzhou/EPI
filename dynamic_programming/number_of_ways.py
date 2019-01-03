2f9be45df25eb393146e9f3571789539112de18c
felix990302 <felix990302@yahoo.ca>
2019-01-02 21:46:34 -0800

    number of ways ot traverse matrix

---
+++ dynamic_programming/number_of_ways.py
@@ -0,0 +1,12 @@
+def number_of_ways(n, m):
+    def compute_number_or_ways_to_xy(x, y):
+        if x == y == 0:
+            return 1
+        elif number_of_ways[x][y] == 0:
+            ways_top = 0 if x == 0 else compute_number_or_ways_to_xy(x-1, y)
+            ways_left = 0 if y ==0 else compute_number_or_ways_to_xy(x, y-1)
+            number_of_ways[x][y] = ways_top + ways_left
+        return number_of_ways[x][y]
+
+    number_of_ways = [[0]*m for  _ in range(n)]
+    return compute_number_or_ways_to_xy(n-1, m-1)
